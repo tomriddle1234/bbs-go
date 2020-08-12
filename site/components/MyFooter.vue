5ce3ff93ef4f6465a9c48901f31335ac127f2e45
tomriddle_1234 <tomriddle_1234@hotmail.com>
2020-08-12 16:13:03 +0800

    Change Beian

--- site/components/MyFooter.vue
+++ site/components/MyFooter.vue
@@ -8,9 +8,9 @@
           <a href="/links">友链</a>
         </div>
         <div>
-          © 2019 Powered by
-          <a href="http://docs.bbs-go.com" target="_blank" class="light"
-            >BBS-GO</a
+          Copyright ©2017-2020 版权所有 桂ICP备17000524号 Powered by
+          <a href="http://vfxware.com" target="_blank" class="light"
+            >VFXWARE</a
           >
         </div>
       </div>
