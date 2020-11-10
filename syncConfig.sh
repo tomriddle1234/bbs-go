bf79686d7958ce2b82ecb736128c871749ceeb43
tomriddle_1234 <tomriddle_1234@hotmail.com>
2020-11-10 17:41:24 +0800

    Fixed syn script

--- syncConfig.sh
+++ syncConfig.sh
@@ -1,1 +1,1 @@
-scp server/bbs-go-wencang.yaml tom@monster:/home/tom/src/bbs-go-wencang/server/bbs-go.yaml
+scp server/bbs-go-wencang.yaml tom@monster-new:/home/tom/src/bbs-go-wencang/server/bbs-go.yaml
